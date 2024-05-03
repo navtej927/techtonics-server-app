9c769217db392cc49b5f69016135f7c9201e4abf
navtej927 <navtej927@gmail.com>
2024-05-02 23:54:27 -0700

    sentry added

---
+++ next.config.js
@@ -0,0 +1,52 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+
+const { withSentryConfig } = require("@sentry/nextjs");
+
+// your existing module.exports or default export
+const nextConfig = {
+
+  // Optional build-time configuration options
+  sentry: {
+    // See the sections below for information on the following options:
+    //   'Configure Source Maps':
+    //     - disableServerWebpackPlugin
+    //     - disableClientWebpackPlugin
+    //     - hideSourceMaps
+    //     - widenClientFileUpload
+    //   'Configure Legacy Browser Support':
+    //     - transpileClientSDK
+    //   'Configure Serverside Auto-instrumentation':
+    //     - autoInstrumentServerFunctions
+    //     - excludeServerRoutes
+    //   'Configure Tunneling to avoid Ad-Blockers':
+    //     - tunnelRoute
+    //   'Disable the Sentry SDK Debug Logger to Save Bundle Size':
+    //     - disableLogger
+  },
+};
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, configFile, stripPrefix, urlPrefix, include, ignore
+
+  org: "techtonics",
+  project: "techtonics-server-app",
+
+  // An auth token is required for uploading source maps.
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
+  silent: true, // Suppresses all logs
+
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+
+// If you're using a next.config.mjs file:
+// export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
